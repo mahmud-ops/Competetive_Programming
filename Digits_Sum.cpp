c7a833945b2ad07273dd479b6bbe0ee5f06a8bc8
Abdullah Al Mahmud <abdullahmahmud01798@gmail.com>
2025-07-20 10:18:09 +0600

    new

---
+++ Digits_Sum.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int t; cin >> t;
+    while(t--) {
+        int n;
+        cin >> n;
+
+        int ans = n/10;
+        if(n % 10 == 9){
+            ans += 1;
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
