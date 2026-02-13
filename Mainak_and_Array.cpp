34c41722d13673b1d04184e6b9bb6109d40b7236
Abdullah Al Mahmud <abdullahmahmud01798@gmail.com>
2026-02-13 20:04:34 +0600

    Approach-3(AC)

--- Mainak_and_Array.cpp
+++ Mainak_and_Array.cpp
@@ -1,24 +1,31 @@
-#include<bits/stdc++.h>
-typedef long long int ll;
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
+	int t;
+	cin >> t; 
+	while (t--)
+	{
+		long long n;
+		cin >> n; 
+		vector<long long> v(n);
+		for (long long i = 0; i < n; i++) 
+			cin >> v[i];
 
-    int t; cin >> t;
-    while(t--) {
-        ll n;
-        cin >> n;
+		long long answer = v[n - 1] - v[0];
 
-        set<ll> a;
+		for (int i = 1; i < n; i++)
+			answer = max(answer, v[i] - v[0]);
 
-        for(int i = 0 ; i < n ; i++){
-            ll val;
-            cin >> val;
+		for (int i = 0; i < n - 1; i++)
+			answer = max(answer, v[n - 1] - v[i]);
 
-            a.insert(val);
-        }
+		for (int i = 0; i < n - 1; i++)
+			answer = max(answer, v[i] - v[i + 1]);
 
-        cout << *a.rbegin() - *a.begin()  << endl;
-    }
-    return 0;
+		cout << answer << endl;
+	}
+	return 0;
 }
+
